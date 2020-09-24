1d86beae086494a25a6a61b2a8b68618dfa94979
Tousif <tousifislampersonal@outlook.com>
2020-09-24 16:51:18 +0530

    Updates

---
+++ precache-manifest.f515fc901b879aa886f85d192b1a8442.js
@@ -0,0 +1,250 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9fc9e7b7eac81e08958967949974638",
+    "url": "/arc-development/index.html"
+  },
+  {
+    "revision": "69189ffa763f3a122b0a",
+    "url": "/arc-development/static/js/2.70e774a7.chunk.js"
+  },
+  {
+    "revision": "14c54f8d621b663941f58f815237ec03",
+    "url": "/arc-development/static/js/2.70e774a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d415d8ea466bab50cdf",
+    "url": "/arc-development/static/js/main.9705fdcb.chunk.js"
+  },
+  {
+    "revision": "603e4dee9a1fe7e513ff",
+    "url": "/arc-development/static/js/runtime-main.37d6018f.js"
+  },
+  {
+    "revision": "350387f5ee9e1569dc00c4952fca7cc8",
+    "url": "/arc-development/static/media/Custom Software Icon.350387f5.svg"
+  },
+  {
+    "revision": "e227d87b2dec28a248985906d655c9ed",
+    "url": "/arc-development/static/media/Footer Adornment.e227d87b.svg"
+  },
+  {
+    "revision": "197792043f0690804982103d4bcf517e",
+    "url": "/arc-development/static/media/analytics.19779204.svg"
+  },
+  {
+    "revision": "ae4fd2b12891f651561a70c0aa0b9ab2",
+    "url": "/arc-development/static/media/android.ae4fd2b1.svg"
+  },
+  {
+    "revision": "1894f630aea694275d0476166f03b826",
+    "url": "/arc-development/static/media/background.1894f630.jpg"
+  },
+  {
+    "revision": "13704aea81946590ce0135f92989a570",
+    "url": "/arc-development/static/media/bell.13704aea.svg"
+  },
+  {
+    "revision": "5deb6d517b57f20ab5414f5b7dd96b96",
+    "url": "/arc-development/static/media/biometrics.5deb6d51.svg"
+  },
+  {
+    "revision": "9938fb3a8e3c4ba27a54b124c51e9125",
+    "url": "/arc-development/static/media/buildIcon.9938fb3a.svg"
+  },
+  {
+    "revision": "0c8991bc318b34e9ff8d78dba3833446",
+    "url": "/arc-development/static/media/bulb.0c8991bc.svg"
+  },
+  {
+    "revision": "9bab166b181f100b2a18683f003930d2",
+    "url": "/arc-development/static/media/camera.9bab166b.svg"
+  },
+  {
+    "revision": "c5e21215338e61508c27fcbee99855a9",
+    "url": "/arc-development/static/media/cash.c5e21215.svg"
+  },
+  {
+    "revision": "8fedc1c48db16c7e3fe5dc868eb12f16",
+    "url": "/arc-development/static/media/check.8fedc1c4.svg"
+  },
+  {
+    "revision": "f66c9bff36e770d5e9cbf5bb63d2652e",
+    "url": "/arc-development/static/media/consultationIcon.f66c9bff.svg"
+  },
+  {
+    "revision": "bbe491c8eed39e557573d8745c005593",
+    "url": "/arc-development/static/media/customized.bbe491c8.svg"
+  },
+  {
+    "revision": "527bb5e49ad56a18f7acd878e761dc8b",
+    "url": "/arc-development/static/media/data.527bb5e4.svg"
+  },
+  {
+    "revision": "5afdf5d46a02a994cfd479d7c5419331",
+    "url": "/arc-development/static/media/designIcon.5afdf5d4.svg"
+  },
+  {
+    "revision": "12e4fb6304961c39cbd06fa45721f36d",
+    "url": "/arc-development/static/media/ecommerce.12e4fb63.svg"
+  },
+  {
+    "revision": "b90c2773b2ac2ed3b98ea0f59dc7d173",
+    "url": "/arc-development/static/media/email.b90c2773.svg"
+  },
+  {
+    "revision": "6323be1c173dd9e2e4007df304745923",
+    "url": "/arc-development/static/media/extendAccess.6323be1c.svg"
+  },
+  {
+    "revision": "c95af783da234448e6b522bd80339aee",
+    "url": "/arc-development/static/media/facebook.c95af783.svg"
+  },
+  {
+    "revision": "1f227262776a7878664d2eef7f931ff8",
+    "url": "/arc-development/static/media/founder.1f227262.jpg"
+  },
+  {
+    "revision": "9f496ff86524fae3b6b5cb0b3dfd3771",
+    "url": "/arc-development/static/media/globe.9f496ff8.svg"
+  },
+  {
+    "revision": "7b7a61a313e51403d5387bb8ad5ca8bb",
+    "url": "/arc-development/static/media/gps.7b7a61a3.svg"
+  },
+  {
+    "revision": "84deaba55d2b90c888cbb6adbee41c38",
+    "url": "/arc-development/static/media/history.84deaba5.svg"
+  },
+  {
+    "revision": "3da1796662364aee80073a24c121ff51",
+    "url": "/arc-development/static/media/img_0.3da17966.png"
+  },
+  {
+    "revision": "a006de5a23bfb510cd736709ac744d75",
+    "url": "/arc-development/static/media/img_1.a006de5a.png"
+  },
+  {
+    "revision": "5813959c49f8154fedb18c195c2b02e6",
+    "url": "/arc-development/static/media/increaseEngagement.5813959c.svg"
+  },
+  {
+    "revision": "4e509c4d2ccb7a05cf96bc79558b8fe2",
+    "url": "/arc-development/static/media/info.4e509c4d.svg"
+  },
+  {
+    "revision": "e22079d988bd34dee9239a6288b42e7d",
+    "url": "/arc-development/static/media/infoBackground.e22079d9.svg"
+  },
+  {
+    "revision": "8a1f257287cb0615e1a97abefedcaaac",
+    "url": "/arc-development/static/media/instagram.8a1f2572.svg"
+  },
+  {
+    "revision": "2e0ff4a3fefcdb65763965d0c2bfdcad",
+    "url": "/arc-development/static/media/iphone.2e0ff4a3.svg"
+  },
+  {
+    "revision": "5ef2a613336c410a0c3866240a0c7c2c",
+    "url": "/arc-development/static/media/iterateIcon.5ef2a613.svg"
+  },
+  {
+    "revision": "5bad4225e28840f42b1fd2fe1735bd5a",
+    "url": "/arc-development/static/media/launchIcon.5bad4225.svg"
+  },
+  {
+    "revision": "ea0de06c0dcabac997a9b8f5605ee00b",
+    "url": "/arc-development/static/media/logo.ea0de06c.svg"
+  },
+  {
+    "revision": "1a97a49efeb526381ab033d7927173a0",
+    "url": "/arc-development/static/media/maintainIcon.1a97a49e.svg"
+  },
+  {
+    "revision": "eb953523f70c3832f9751c1081913138",
+    "url": "/arc-development/static/media/mobileBackground.eb953523.jpg"
+  },
+  {
+    "revision": "6838a9fb85f01988c85e38475ae59fb0",
+    "url": "/arc-development/static/media/mobileIcon.6838a9fb.svg"
+  },
+  {
+    "revision": "ff5529c6b414757e8b0bf50aedaf7204",
+    "url": "/arc-development/static/media/mockupIcon.ff5529c6.svg"
+  },
+  {
+    "revision": "1360c2cf3f702828e1ebc23d29443a3a",
+    "url": "/arc-development/static/media/outreach.1360c2cf.svg"
+  },
+  {
+    "revision": "00650db5424c203dc49eb2b0b0a04b78",
+    "url": "/arc-development/static/media/people.00650db5.svg"
+  },
+  {
+    "revision": "361cfe9636e9b4e2f8a4753b7c16de4d",
+    "url": "/arc-development/static/media/person.361cfe96.svg"
+  },
+  {
+    "revision": "ab1e4513af40f9fe5fd2f462b803bacc",
+    "url": "/arc-development/static/media/persons.ab1e4513.svg"
+  },
+  {
+    "revision": "171f2b32ae323f5d2692cb2927d55283",
+    "url": "/arc-development/static/media/phone.171f2b32.svg"
+  },
+  {
+    "revision": "4d2e310400290a95679fd23d80d77dfd",
+    "url": "/arc-development/static/media/puppy.4d2e3104.svg"
+  },
+  {
+    "revision": "c608f23f6d6a6d579e2138bd77b6d054",
+    "url": "/arc-development/static/media/repeatingBackground.c608f23f.svg"
+  },
+  {
+    "revision": "6f5512eb8bfb6e76bdd7a30f473403e2",
+    "url": "/arc-development/static/media/reviewIcon.6f5512eb.svg"
+  },
+  {
+    "revision": "3ad96c4bf1627df7cefd64aa7efa5843",
+    "url": "/arc-development/static/media/root.3ad96c4b.svg"
+  },
+  {
+    "revision": "cdaac4cce3754b447a32bbaf811bec48",
+    "url": "/arc-development/static/media/send.cdaac4cc.svg"
+  },
+  {
+    "revision": "72de4d77fdcd3cf6f2c8171f8aa0e8b3",
+    "url": "/arc-development/static/media/seo.72de4d77.svg"
+  },
+  {
+    "revision": "9ce632eb2f76db1534b07869a48b59fc",
+    "url": "/arc-development/static/media/stopwatch.9ce632eb.svg"
+  },
+  {
+    "revision": "18d93704942fa030299061605654ef7d",
+    "url": "/arc-development/static/media/swissKnife.18d93704.svg"
+  },
+  {
+    "revision": "13b0990004faa8299dd60e83b2b12569",
+    "url": "/arc-development/static/media/twitter.13b09900.svg"
+  },
+  {
+    "revision": "154306d8687349147c3ab1a0f91cbf22",
+    "url": "/arc-development/static/media/upload.154306d8.svg"
+  },
+  {
+    "revision": "1c804276b5fe36de0974c6e15ef817df",
+    "url": "/arc-development/static/media/users.1c804276.svg"
+  },
+  {
+    "revision": "01af09aaeb87364effb3ae57d70a92e0",
+    "url": "/arc-development/static/media/vision.01af09aa.svg"
+  },
+  {
+    "revision": "6b90d7dca8c7d0d5340cc0748c6bdb3c",
+    "url": "/arc-development/static/media/websiteIcon.6b90d7dc.svg"
+  },
+  {
+    "revision": "ad8a12c115fa73d4b87417b378f8e69e",
+    "url": "/arc-development/static/media/yearbook.ad8a12c1.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.920048f665bffc61fd7af1f09bc4f470.js"
+  "/arc-development/precache-manifest.f515fc901b879aa886f85d192b1a8442.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/arc-development/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
